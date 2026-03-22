18266e159d83d126644a22aa5f0fd8648d5ee295
morhansa <moakamok@gmail.com>
2026-03-23 00:43:38 +0200

    Update custom.js

--- assets/user/js/custom.js
+++ assets/user/js/custom.js
@@ -750,25 +750,6 @@ $(function ($) {
         let action = $('#itemForm').attr('action');
         let fd = new FormData(document.querySelector('#itemForm'));
 
-        // Ensure Summernote/TinyMCE content is in FormData (blog create/edit: avoid undefined or stale textarea)
-        if ($('#itemForm').find('.summernote').length > 0) {
-            $('#itemForm').find('.summernote').each(function (i) {
-                let $el = $(this);
-                let name = $el.attr('name');
-                let content = '';
-                let tmcId = $el.attr('id');
-                if (tmcId && typeof tinyMCE !== 'undefined' && tinyMCE.get(tmcId)) {
-                    content = tinyMCE.get(tmcId).getContent() || '';
-                } else {
-                    content = $el.val() || '';
-                }
-                if (name) {
-                    fd.delete(name);
-                    fd.append(name, content);
-                }
-            });
-        }
-
         let blob_image_url = $('#blob_image').text().trim();
         if (blob_image_url.length > 0) {
             var base64ImageContent = blob_image_url.replace(/^data:image\/(png|jpg);base64,/, "");
@@ -874,7 +855,7 @@ $(function ($) {
     /* ***************************************************
     ==========Form Prepopulate After Clicking Edit Button Start==========
     ******************************************************/
-    $(".editbtn").on('click', function () {
+    $(document).off('click.turboEditbtn', '.editbtn').on('click.turboEditbtn', '.editbtn', function () {
         let datas = $(this).data();
         delete datas['toggle'];
 
@@ -929,7 +910,7 @@ $(function ($) {
     /* ***************************************************
     ==========Form Update with AJAX Request Start==========
     ******************************************************/
-    $("#updateBtn").on('click', function (e) {
+    $(document).off('click.turboUpdateBtn', '#updateBtn').on('click.turboUpdateBtn', '#updateBtn', function (e) {
 
         $(".request-loader").addClass("show");
 
